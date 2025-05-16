5bc50a59d0695c559c4a86c5a10798cc3f2094dc
iccarvalho <igccarvalho06@outlook.com>
2025-05-15 21:21:49 -0300

    exe01

--- estruturaRepeticao/listaExRepeticao/exe01/scripts.js
+++ estruturaRepeticao/listaExRepeticao/exe01/scripts.js
@@ -1,9 +1,9 @@
 function exe01() {
     let a, b, c, d, aux;
     let grupo = 1;
-    let count = 1;
 
     while (grupo <= 5) {
+        let count = 1;
         a = Number(prompt(`Informe o valor de A`))
         b = Number(prompt(`Informe o valor de B`))
         c = Number(prompt(`Informe o valor de C`))
