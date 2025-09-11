21401993db5304a4d3e779c3c49d3b58b9e34db2
iccarvalho <igccarvalho06@outlook.com>
2025-09-10 22:57:44 -0300

    correcao exercicios

--- beecrowd/1179.js
+++ beecrowd/1179.js
@@ -1,12 +1,12 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
+var lines = input.trim().split('\n');
 
 let par = [];
 let impar = [];
 
 for(let i = 0; i < lines.length; i++){
-    if(lines[i] % 2 === 0){
-        par.push(lines[i]);
+    if(parseInt(lines[i]) % 2 === 0){
+        par.push(parseInt(lines[i]));
         if(par.length == 5){
             for(let j = 0; j < par.length; j++){
                 console.log(`par[${j}] = ${par[j]}`);
@@ -14,7 +14,7 @@ for(let i = 0; i < lines.length; i++){
             par = [];
         }
     } else{
-        impar.push(lines[i]);
+        impar.push(parseInt(lines[i]));
         if(impar.length == 5){
             for(let j = 0; j < impar.length; j++){
                 console.log(`impar[${j}] = ${impar[j]}`);
